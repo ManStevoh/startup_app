2232275c425b90420c8dd23565d190cb8307159a
ManStevoh <stephenmusyoka207@gmail.com>
2025-02-02 12:24:54 -0800

    tail wind config file correctly imported

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,63 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+// Import your local font using the helper from next/font/local
+const workSans = localFont({
+  src: [
+    { 
+      path: './fonts/workSans-Black.ttf',
+      weight: '900',
+      style: 'normal',
+    },
+    { 
+      path: './fonts/workSans-extraBold.ttf',
+      weight: '800',
+      style: 'normal',
+    },
+    { 
+      path: './fonts/workSans-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    { 
+      path: './fonts/workSans-SemiBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    { 
+      path: './fonts/workSans-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    { 
+      path: './fonts/._WorkSans-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    { 
+      path: './fonts/workSans-Thin.ttf',
+      weight: '200',
+      style: 'normal',
+    },
+    { 
+      path: './fonts/workSans-ExtraLight.ttf',
+      weight: '100',
+      style: 'normal',
+    }
+  ],
+  variable: '--font-work-sans',
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+// Initialize your Google fonts. (Adjust options as needed)
+const geistSans = Geist({
   subsets: ["latin"],
+  variable: '--font-geist-sans',
+});
+const geistMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: '--font-geist-mono',
 });
 
 export const metadata: Metadata = {
@@ -19,16 +67,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${workSans.variable} antialiased`}>
         {children}
       </body>
     </html>
   );
+  
 }
